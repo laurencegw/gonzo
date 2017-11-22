e66c78a66ceb9d88b61c93bfee01ba6cf8707772
Laurence Willmore <laurence_willmore@yahoo.co.uk>
2017-11-22 23:19:44 +0000

    Starting with jpa

--- web-server/src/main/kotlin/com/binarymonks/me/core/blog/persistence/persistence.kt
+++ web-server/src/main/kotlin/com/binarymonks/me/core/blog/persistence/persistence.kt
@@ -1,13 +1,35 @@
 package com.binarymonks.me.core.blog.persistence
 
+import org.hibernate.annotations.UpdateTimestamp
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.repository.CrudRepository
 import java.time.ZonedDateTime
+import javax.persistence.*
 
+@Entity
 data class BlogEntry(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         var id: Long? = null,
+
+        @Column(nullable = false)
         var title: String,
+
+        @Lob
+        @Column(nullable = false)
         var content: String,
+
+        @Column(nullable = false)
         var published: Boolean,
+
+        @Column(nullable = true)
         var firstPublished: ZonedDateTime? = null,
+
+        @CreatedDate
         var created: ZonedDateTime,
+
+        @UpdateTimestamp
         var updated: ZonedDateTime
 )
+
+interface BlogRepo : CrudRepository<BlogEntry, Long>
