6d8c02ae7c807b770e82768ea0c64282f649ddb8
Laurence <laurence.willmore@magnetic.com>
2018-08-03 08:29:20 +0100

    Got text area working down to store on edit blog

--- vue-client/src/components/VTextArea.vue
+++ vue-client/src/components/VTextArea.vue
@@ -1,23 +1,30 @@
 <template>
-  <input
+  <textarea
       :value="value"
-      type="text"
+      :rows="rows"
+      :cols="cols"
       @keyup="keyUp"
-  >
+  ></textarea>
 </template>
 
 <script lang="ts">
     import {Component, Prop, Vue} from "vue-property-decorator"
 
     @Component({
-        name: "v-input"
+        name: "v-text-area"
     })
-    export default class VInput extends Vue {
+    export default class VTextArea extends Vue {
         @Prop({default: ""})
         value!: string
 
         @Prop({default: "unknown"})
         attributeName!: string
+
+        @Prop({default: "5"})
+        rows!: string
+
+        @Prop({default: "50"})
+        cols!: string
 
         keyUp(event) {
             this.$emit("keyup", {attributeName: this.attributeName, value: event.target.value})
--- vue-client/src/mycontent/EditBlog.vue
+++ vue-client/src/mycontent/EditBlog.vue
@@ -1,25 +1,33 @@
 <template>
-  <v-loading :is-loading="!loaded">
-    <b-container fluid>
-      <b-row>
-        <b-col md="1"><label for="title-input">Title:</label></b-col>
-        <b-col md="9">
-          <v-input
-              :value="title"
-              id="title-input"
-              attribute-name="title"
-              type="text"
-              @keyup="attributeChange"
-          ></v-input>
-        </b-col>
-        <b-col md="2">
-          <v-button @click="save()">Save</v-button>
-        </b-col>
-      </b-row>
-      <br>
-      <b-row><textarea :value="content" @keyup="attributeChange" rows="15" cols="80"></textarea></b-row>
-    </b-container>
-  </v-loading>
+    <v-loading :is-loading="!loaded">
+        <b-container fluid>
+            <b-row>
+                <b-col md="1"><label for="title-input">Title:</label></b-col>
+                <b-col md="9">
+                    <v-input
+                            attribute-name="title"
+                            :value="title"
+                            id="title-input"
+                            type="text"
+                            @keyup="attributeChange"
+                    ></v-input>
+                </b-col>
+                <b-col md="2">
+                    <v-button @click="save()">Save</v-button>
+                </b-col>
+            </b-row>
+            <br>
+            <b-row>
+                <v-text-area
+                        attribute-name="content"
+                        :value="content"
+                        @keyup="attributeChange"
+                        rows=15
+                        cols=80
+                ></v-text-area>
+            </b-row>
+        </b-container>
+    </v-loading>
 
 </template>
 <script lang="ts">
@@ -30,9 +38,11 @@
     import {BlogDraft} from "@/blogs/api"
     import VLoading from "@/components/VLoading.vue"
     import VInput from "@/components/VInput.vue"
+    import VTextArea from "../components/VTextArea.vue"
 
     @Component({
         components: {
+            VTextArea,
             VButton,
             VLoading,
             VInput
