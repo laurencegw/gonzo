3ec941635f99d0dbc52beee4247c39748b8cccf8
LaurenceW <lgwillmore@gmail.com>
2018-08-19 13:20:18 +0100

    Starting to build read article view

---
+++ vue-client/src/views/ReadArticle.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-hunter-quote>Life has become immeasurably better since I have been forced to stop taking it seriously.
+    </v-hunter-quote>
+</template>
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator"
+    import VHunterQuote from "@/components/VHunterQuote.vue"
+
+    @Component({
+        components: {VHunterQuote}
+    })
+    export default class ReadArticle extends Vue {
+    }
+</script>
